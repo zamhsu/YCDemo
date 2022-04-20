da97dbf3448037083da45b650aae92fe23cab753
zamhsu <41725143+zamhsu@users.noreply.github.com>
2022-04-20 22:51:26 +0800

    加入Swagger和EF Core套件

--- src/WebApi/WebApi/Program.cs
+++ src/WebApi/WebApi/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 app.Run();
