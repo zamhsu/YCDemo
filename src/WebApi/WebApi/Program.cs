fa2c55dfc97c90a720cec2c0493e8419dcc19dcd
zamhsu <41725143+zamhsu@users.noreply.github.com>
2022-04-20 23:31:55 +0800

    加入資料庫連接字串

--- src/WebApi/WebApi/Program.cs
+++ src/WebApi/WebApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<YCDemoContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")));
+builder.Services.AddScoped<DbContext, YCDemoContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
