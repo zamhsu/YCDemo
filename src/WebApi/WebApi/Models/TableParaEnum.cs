372c9d9216b90196532fcb0144812f611a1d930c
zamhsu <41725143+zamhsu@users.noreply.github.com>
2022-04-21 22:07:43 +0800

    加入狀態參數enum

---
+++ src/WebApi/WebApi/Models/TableParaEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApi.Models
+{
+    public enum ProductStatusEnum
+    {
+        Delete,
+        Ok
+    }
+
+    public enum CartStatusEnum
+    {
+        Delete,
+        Ok
+    }
+
+    public enum OrderStatusEnum
+    {
+        Delete,
+        Ok
+    }
+}
